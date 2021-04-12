37c187880809f09a13280a39c750b498efe169a8
lannagif <34742332+lannagif@users.noreply.github.com>
2021-04-12 16:55:03 -0300

    Update WEBserver.ino

--- InGateway/WEBserver/WEBserver.ino
+++ InGateway/WEBserver/WEBserver.ino
@@ -43,19 +43,3 @@ void loop()
 }
 
 
-/* ----------------------------------------------------------------
-  1- setup
-  1.0 - Verifica se está configurado, se não continua
-  1.1 - Habilita AP com nome =>  InGatewayWifi
-  1.2 - Habilita server http
-  1.2 - Flutter envia dados de login ( POST/GET http://GATEWAY_AP/config?uid={UID}&ssid={SSID}&pw={PASS_WORD} ) [gateway_ap=192.168.125.1]
-    - UID => não necessita passwd e usermail
-    - wifiSsid
-    - wifi_passwd
-  1.3 - Marca rotina de configuração OK
-  1.4 - Inicializa sistema
-
-  2- Coleta dispositivos via Firebase
-
-  3- Atualiza portas do ESP32 com o dado do Firebase
-  --------------------*/
